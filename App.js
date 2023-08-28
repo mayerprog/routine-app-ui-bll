43c7a3e702c123e8d83ea1ccf5263c216e0da637
mayerprog <supermayerehs@gmail.com>
2023-08-28 11:43:37 +0300

    added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { AuthContext, AuthProvider } from "./src/context/AuthContext";
 import AppNav from "./src/navigation/AppNav";
-
+// changes
 export default function App() {
   return (
     <AuthProvider>
